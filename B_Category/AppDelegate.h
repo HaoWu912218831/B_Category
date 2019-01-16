4898012d2a8f56d210bbdee333d11b55610d081f
912218831 <912218831@qq.com>
2019-01-16 16:44:10 +0800

    Initial Commit

---
+++ B_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  B_Category
+//
+//  Created by 杨庆龙 on 2019/1/16.
+//  Copyright © 2019年 杨庆龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ B_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  B_Category
+//
+//  Created by 杨庆龙 on 2019/1/16.
+//  Copyright © 2019年 杨庆龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
